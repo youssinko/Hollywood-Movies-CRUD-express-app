8de62b56ca512b0a1eebd10629ff7ec8fab5c0e2
Rania Elnaggar <yousef_amir1@hotmail.com>
2022-12-01 09:11:57 -0500

    first draft

---
+++ views/movies/Index.jsx
@@ -0,0 +1,28 @@
+const React= require('react')
+const DefaultLayout = require('../Default')
+//deconstructing React which means ( pull me only "component class" from react)
+class Index extends React.Component {
+    render(){
+      const { movies } = this.props
+      return(
+        <DefaultLayout>
+          <div>
+            <a href="/movies/new">Create a Movie</a>
+            {
+              movies.map((movie) => {
+                return(
+                  <article>
+                    <img src={movie.poster} alt="" />
+                    <a href={`/movies/${movie._id}`}><h2>{movie.title}</h2></a>
+        
+                  </article>
+                )
+              })
+            }
+          </div>
+        </DefaultLayout>
+      )
+    }
+  }
+  
+  module.exports = Index
